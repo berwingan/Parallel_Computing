ae1105e3e0fa1f6ed3b7441e3b683a32e42a985d
berwingan <wqg203@nyu.edu>
2019-10-26 00:56:04 -0400

    Update seqgenprimes.c

--- Lab1-PrimeNumbers/seqgenprimes.c
+++ Lab1-PrimeNumbers/seqgenprimes.c
@@ -4,18 +4,19 @@
 
 int main(int argc, char *argv[])
 {
+
     FILE *file;
     char filename[32];
     strcpy(filename, argv[1]);
     strcat(filename, ".txt");
 
-    int N = atoi(argv[1]);
+    int N = atoi(argv[1]); //number to count to
     int floor = ((N + 1) / 2);
-    int arr_a[N];
+    int *arr_a = (int *)malloc((N + 1) * sizeof(int)); //+ 1 for 0
+    //int arr_a[N];
 
     arr_a[0] = 0; // for 1
     arr_a[1] = 0; // for 0
-    int length = sizeof(arr_a) / sizeof(arr_a[0]);
     int i;
     for (i = 2; i <= N; i++) //generate all number from 2 to N in array(arr)
     {
@@ -27,15 +28,15 @@ int main(int argc, char *argv[])
     {
         if (arr_a[i] == 0)
         {
-            continue;
+            continue; //loop
         }
         else if (arr_a[i] > floor)
         {
-            break;
+            break; //done with
         }
         int prime = arr_a[i];
         int multiple;
-        for (multiple = prime + prime; multiple < (N + 1); multiple += prime)
+        for (multiple = prime + prime; multiple < (N + 1); multiple += prime) //first one is double
         {
             arr_a[multiple] = 0;
         }
@@ -43,7 +44,7 @@ int main(int argc, char *argv[])
     //till here
 
     file = fopen(filename, "w"); //write to txt here
-    for (i = 0; i < length + 1; i++)
+    for (i = 0; i < N + 1; i++)
     {
         if (arr_a[i] != 0)
         {
@@ -51,5 +52,7 @@ int main(int argc, char *argv[])
         }
     }
     fclose(file);
+
+    free(arr_a); //freeing malloc here
     return 0;
 }
